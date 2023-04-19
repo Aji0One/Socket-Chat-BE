758ecf39cb7aa7fb155e823cccc9e0f777ad4f71
Aji0One <109173463+Aji0One@users.noreply.github.com>
2023-04-19 15:39:07 +0530

    link added properly

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "https://poetic-souffle-a760f0.netlify.app/chat",
+    origin: "https://poetic-souffle-a760f0.netlify.app",
   },
 });
 
